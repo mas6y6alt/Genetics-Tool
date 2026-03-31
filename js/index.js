d665b52528791eec96faaaade2246f398f4a44ee
mas6y6alt <222933@mystisd.net>
2026-03-31 10:27:04 -0500

    Update navigation paths for monohybrid and dihybrid

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded",() => {
 
     monohybrid_button = document.getElementById("monohybrid");
     monohybrid_button.addEventListener("click",() => {
-        window.location.href = "/monohybrid.html"
+        window.location.href = "/Genetics-Tool/monohybrid.html"
     });
 
     monohybrid_button = document.getElementById("dihybrid");
     monohybrid_button.addEventListener("click",() => {
-        window.location.href = "/dihybrid.html"
+        window.location.href = "/Genetics-Tool/dihybrid.html"
     });
 
 });
